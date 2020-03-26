f9eb646ecfe5a86403615dbb042dbb2f369a1744
gushucai <875550199@qq.com>
2020-03-26 19:47:40 +0800

    加入用户管理模块

---
+++ gmall-user-manager/src/main/java/com/atguigu/gmall/user/GmallUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserManagerApplication.class, args);
+    }
+
+}
