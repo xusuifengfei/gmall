219d9006b830e5d4d75576cf7fbcc681ac3a08ef
gsc <875550199@qq.com>
2020-04-02 10:24:02 +0800

    juc高并发之CountDownLatch 减少计数

--- gmall-user-manager/src/main/java/com/atguigu/juc/CountDownLatchDemo.java
+++ gmall-user-manager/src/main/java/com/atguigu/juc/CountDownLatchDemo.java
@@ -11,6 +11,33 @@ import java.util.concurrent.CountDownLatch;
  * @date 2020/4/2 0002 9:55
  * @Description 减少基数
  */
+enum CountryEnum{
+    ONE(1,"齐"),TWO(2,"楚"),THREE(3,"燕"),FOUR(4,"赵"),FIVE(5,"魏"),SIX(6,"韩");
+    private Integer retuInt;
+    private String retuMessage;
+    private CountryEnum(Integer retuInt ,String retuMessage){
+        this.retuInt = retuInt;
+        this.retuMessage = retuMessage;
+    }
+
+    public Integer getRetuInt() {
+        return retuInt;
+    }
+
+    public String getRetuMessage() {
+        return retuMessage;
+    }
+
+    public static CountryEnum forEach_CountryEnum(Integer retuInt){
+        CountryEnum[] countryEnums = CountryEnum.values();
+        for (CountryEnum countryEnum : countryEnums) {
+            if (retuInt.equals(countryEnum.getRetuInt())){
+                return countryEnum;
+            }
+        }
+        return null;
+    }
+}
 public class CountDownLatchDemo {
 
     @Test
@@ -21,6 +48,18 @@ public class CountDownLatchDemo {
             },""+i).start();
         }
         System.out.println(Thread.currentThread().getName()+"\t"+"班长锁教室门");
+    }
+    @Test
+    public void testAfter() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(5);
+        for (int i = 1; i <= 6; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"\t"+"国被灭");
+                latch.countDown();
+            },CountryEnum.forEach_CountryEnum(i).getRetuMessage()).start();
+        }
+        latch.await();
+        System.out.println(Thread.currentThread().getName()+"\t"+"秦国一统华夏");
     }
 
 
@@ -34,6 +73,5 @@ public class CountDownLatchDemo {
         }
         latch.await();
         System.out.println(Thread.currentThread().getName()+"\t"+"班长锁教室门");
-
     }
 }
